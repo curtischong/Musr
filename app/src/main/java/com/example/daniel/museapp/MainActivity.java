e98e82182e09c4eb622f24d17c0d54a27b90f7bf
Marcel O'Neil <marcel@marceloneil.com>
2017-08-26 20:30:39 -0400

    Remove log

--- app/src/main/java/com/example/daniel/museapp/MainActivity.java
+++ app/src/main/java/com/example/daniel/museapp/MainActivity.java
@@ -185,7 +185,7 @@ public class MainActivity extends Activity implements OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        fft = new FFT(256,256,256);
+        fft = new FFT(256, 256, 256);
 
         // We need to set the context on MuseManagerAndroid before we can do anything.
         // This must come before other LibMuse API calls as it also loads the library.
@@ -481,13 +481,6 @@ public class MainActivity extends Activity implements OnClickListener{
 
         buffer[4] = p.getEegChannelValue(Eeg.AUX_LEFT);
         buffer[5] = p.getEegChannelValue(Eeg.AUX_RIGHT);
-
-        //PostExample example = new PostExample();
-        long newTime = System.currentTimeMillis();
-        for (double item : buffer) {
-            Log.i("EEG", Double.toString(item));
-        }
-
     }
 
     private void getAccelValues(MuseDataPacket p) {
